fb73f3a30cbf4f68c42d7122cb0ff51d3688037d
Jovan Vukic <jovanv0803@gmail.com>
2025-05-08 15:42:34 +0200

    Add Eureka support and API key header to Open Feign requests

--- src/main/java/com/internship/review_service/feign/user/UserService.java
+++ src/main/java/com/internship/review_service/feign/user/UserService.java
@@ -1,13 +1,16 @@
 package com.internship.review_service.feign.user;
 
+import com.internship.authentication_library.feign.interceptor.UserServiceFeignConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "user-service", url = "${microservicesUrls.user-service}")
+@FeignClient(
+        name = "user-service",
+        url = "${microservicesUrls.user-service}",
+        configuration = UserServiceFeignConfiguration.class
+)
 public interface UserService {
-
     @GetMapping("/v1/users/{id}")
     UserDTO getUser(@PathVariable("id") Long id);
-
 }
